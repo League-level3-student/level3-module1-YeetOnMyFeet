877d91d6cf5f75dd9eaa52d7ef38da578db4b33f
Roshan Sood <legodude2774@gmail.com>
2021-02-22 13:30:52 -0800

    finished text undo

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,22 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.lang.reflect.Array;
+import java.util.Stack;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class _02_TextUndoRedo implements KeyListener{
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
@@ -13,6 +26,55 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	Stack<String> stack = new Stack<String>();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	public static void main(String[] args) {
+		new _02_TextUndoRedo().run();
+		
+	}
+	public void run() {
+		frame.add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+		frame.setVisible(true);
+		label.setText("Hello");
+		frame.setSize(500,500);
+		frame.pack();
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyChar());
+		if (e.getKeyChar() != KeyEvent.VK_BACK_SPACE && e.getKeyChar() != KeyEvent.VK_1) {
+			label.setText(label.getText() + e.getKeyChar());
+		}
+		if (KeyEvent.VK_BACK_SPACE == e.getKeyCode()) {
+			System.out.println("Backspace");
+			System.out.println(stack.push(label.getText().substring(label.getText().length() -1)));
+			System.out.println(stack.firstElement());
+			label.setText(label.getText().substring(0,label.getText().length() -1));
+			
+		}
+		if (KeyEvent.VK_1 == e.getKeyChar()) {
+			System.out.println("Undo");
+			stack.pop();
+			label.setText(label.getText() + stack.pop());
+		}
+			
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 }
